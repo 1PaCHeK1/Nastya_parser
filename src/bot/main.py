d39de175e667814038bff4ad6079b1b091192c72
rodionovanastya <rodionovaa.nastya2008@gmail.com>
2023-02-02 20:14:27 +0300

    add sentry

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,7 +1,7 @@
 import argparse
 from aiogram import executor, Dispatcher, types
 from aiogram.utils.executor import start_webhook
-
+import sentry_sdk
 from core.containers import Container
 from core.config import get_config
 from bot.commands import dp
@@ -56,6 +56,11 @@ async def on_shutdown(dp:Dispatcher):
 
 
 def main():
+    sentry_sdk.init(
+        dsn="https://7a7685a757924dc7ad2d539e2a2aec78@o4504611526672384.ingest.sentry.io/4504611528638464",
+        traces_sample_rate=1.0
+        )
+    
     args = arg_parse()
     config = get_config()
 
